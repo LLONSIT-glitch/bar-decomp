741b594abca591f96a618ba0a2fc0d69b0368776
LLONSIT-glitch <dsanabriasierra26@gmail.com>
2026-02-20 21:16:58 -0500

    Match 46F0

--- src/46F0.c
+++ src/46F0.c
@@ -1,6 +1,24 @@
 #include "common.h"
 #include "stdarg.h"
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/46F0/func_80003AF0.s")
+extern s32 D_8002DA80[];
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/46F0/func_80003B64.s")
+void func_80003AF0(s32 arg0, ...) {
+    va_list args;
+    s32 arg;
+
+    va_start(args, arg0);
+    while (arg0 != 0) {
+        if (arg0 >= 0x17) {
+            break;
+        }
+        D_8002DA80[arg0 - 1] = va_arg(args, s32);
+        arg0 = va_arg(args, s32);
+    }
+    
+    va_end(args);
+}
+
+s32 func_80003B64(s32 arg0) {
+    return D_8002DA80[arg0 - 1];
+}
