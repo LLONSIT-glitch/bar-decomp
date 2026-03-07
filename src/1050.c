06beb532880436b6aab9acadbff6370a2c61134f
SynaMax <djsynamax@gmail.com>
2026-03-07 19:01:32 +0000

    added global prefix

--- src/1050.c
+++ src/1050.c
@@ -14,8 +14,8 @@ extern s32 D_80025CDC;
 extern s32 D_80025CE0;
 extern s32 D_80025CE4;
 extern s32 D_80025CE8;
-extern s32 gameStateFlag;
-extern s32 currentGameState;
+extern s32 gGameStateFlag;
+extern s32 gCurrentGameState;
 extern UnkStruct_8002D1A4* D_8002D1A4;
 extern UnkStruct_80025BE8* D_80025BE8;
 extern s32 D_8001F634[];
@@ -42,8 +42,8 @@ void uvSetGameState(s32 gameStateId) {
 
     if (D_80025C08 != NULL) {
         D_80025C08->unk74();
-        if (currentGameState != -1) {
-            D_8002D1A4->unkC(currentGameState);
+        if (gCurrentGameState != -1) {
+            D_8002D1A4->unkC(gCurrentGameState);
         }
         temp_v0 = func_800015D4('UVMO', func_80003494('MIDI'));
         for (var_s0 = 0; var_s0 < temp_v0; var_s0++) {
@@ -69,8 +69,8 @@ void uvSetGameState(s32 gameStateId) {
         }
 
     }
-    currentGameState = gameStateId;
-    gameStateFlag = -1;
+    gCurrentGameState = gameStateId;
+    gGameStateFlag = -1;
     uvSysInit();
     D_80025C98 = 0;
     D_80025C9C = 0;
