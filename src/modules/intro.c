945bbf2018d3efe802e0f28aedf409953597b224
SynaMax <11140222+synamaxmusic@users.noreply.github.com>
2026-04-17 19:42:01 +0000

    Added `intro.h`

--- src/modules/intro.c
+++ src/modules/intro.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "common.h"
 #include "module.h"
+#include "intro.h"
 
 typedef struct UnkStruct_80025BD8_s {
     char pad0[0x14];
@@ -85,12 +86,6 @@ typedef struct UnkStruct_intro_00400A84_s {
     void (*unkC)(struct UnkPlayer*);
     char pad24[0x10];
 } UnkStruct_intro_00400A84;
-
-typedef struct Intro_Exports_s {
-    /* 0x00 */ void (*func_intro_004004F0)(void);                      /* inferred */
-    /* 0x04 */ void (*func_intro_004005CC)(void);
-    /* 0x08 */ void (*func_intro_00400820)(void);
-} Intro_Exports;
 
 typedef struct UnkStruct_80025C88_s {
     /* 0x00 */ void (*unk0)(void);                      /* inferred */
