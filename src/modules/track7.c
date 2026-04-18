8d7fcd051944958de16dbce334efa714d6476599
SynaMax <11140222+synamaxmusic@users.noreply.github.com>
2026-04-18 06:11:07 +0000

    Fix

--- src/modules/track7.c
+++ src/modules/track7.c
@@ -2,7 +2,7 @@
 #include "common.h"
 #include "module.h"
 
-extern s32 D_004000B0;
+extern s32 D_track7_004000B0;
 extern void func_track7_00400094(void);
 extern void func_track7_0040009C(void);
 extern UnkStruct_80025CF0 gGameSettings[];
@@ -12,7 +12,7 @@ void __entrypoint_func_track7_400000(Track7_Exports *exports) {
     exports->func_track7_00400094 = &func_track7_00400094;
     exports->func_track7_0040009C = &func_track7_0040009C;
     #line 1
-    gGameSettings->unkF0 = &D_004000B0;
+    gGameSettings->unkF0 = &D_track7_004000B0;
     D_80025CB0 = uvLoadModule('tdta');
     if (gCurrentGameState != 0xA) {
         D_80025CB0->unk24();
