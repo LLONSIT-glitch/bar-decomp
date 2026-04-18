7096ef9c816eb28191aa91a5e6ff507f733dbd4e
SynaMax <11140222+synamaxmusic@users.noreply.github.com>
2026-04-18 05:52:14 +0000

    Cleanup

--- src/modules/track7.c
+++ src/modules/track7.c
@@ -7,24 +7,22 @@ typedef struct UnkStruct_80025CB0_s {
     void (*unk24)(void);
 } UnkStruct_80025CB0;
 
-void* func_80003520(s32, s32);                        /* extern */
-void func_80003760(s32);                                 /* extern */
 extern s32 D_004000B0;
 extern void func_track7_00400094(void);
 extern void func_track7_0040009C(void);
 extern UnkStruct_80025CF0 gGameSettings[];
     
-void __entrypoint_func_track7_400000(Track7_Exports *arg0) {
-    uvSetFileDirOvlPtr(arg0);
-    arg0->func_track7_00400094 = &func_track7_00400094;
-    arg0->func_track7_0040009C = &func_track7_0040009C;
+void __entrypoint_func_track7_400000(Track7_Exports *exports) {
+    uvSetFileDirOvlPtr(exports);
+    exports->func_track7_00400094 = &func_track7_00400094;
+    exports->func_track7_0040009C = &func_track7_0040009C;
     #line 1
     gGameSettings->unkF0 = &D_004000B0;
-    D_80025CB0 = func_80003520('tdta', arg0);
+    D_80025CB0 = uvLoadModule('tdta', exports);
     if (gCurrentGameState != 0xA) {
         D_80025CB0->unk24();
     }
-    func_80003760('tdta');
+    uvUnloadModule('tdta');
     D_80025CB0 = NULL;
 }
 
