3a8b458bb35d9fddb05ef6cc73e3607f299436de
SynaMax <11140222+synamaxmusic@users.noreply.github.com>
2026-04-18 05:08:48 +0000

    Track 7 exports and header file

---
+++ include/track7.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+#ifndef BAR_TRACK7_H
+#define BAR_TRACK7_H
+
+typedef struct Track7_Exports_s {
+    /* 0x00 */ void (*PLACEHOLDER)(void);                      /* inferred */
+    /* 0x04 */ void (*PLACEHOLDER)(void);
+    /* 0x08 */ void (*PLACEHOLDER)(void);
+} Track7_Exports;
+
+#endif /* BAR_TRACK7_H */
--- src/modules/track7.c
+++ src/modules/track7.c
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
+#include "common.h"
+#include "module.h"
+
 #pragma GLOBAL_ASM("asm/us/nonmatchings/modules/track7/__entrypoint_func_track7_400000.s")
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/modules/track7/func_track7_00400094.s")
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/modules/track7/func_track7_0040009C.s")
-
