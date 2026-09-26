497857f908b50f972eff95b3b4bd8b089e7e9c57
LLONSIT-glitch <LLONSIT-glitch@users.noreply.github.com>
2026-09-25 22:24:16 -0500

    Add uvmodel_rom.h

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ find-command = $(shell which $(1) 2>/dev/null)
 # If COMPARE is 1, check the output sha1 after building
 COMPARE ?= 1
 # If NON_MATCHING is 1, define the NON_MATCHING C flag when building
-NON_MATCHING ?= 0
+NON_MATCHING ?= 1
 # if WERROR is 1, pass -Werror to CC_CHECK, so warnings would be treated as errors
 WERROR ?= 0
 # Keep .mdebug section in build
--- src/modules/weapon.c
+++ src/modules/weapon.c
@@ -3,6 +3,8 @@
 #include "module.h"
 #include "global_exports.h"
 
+#define WEAPON_COUNT 12
+
 extern UnkStruct_weapon_00400184* D_weapon_00400184;
 extern s32 D_weapon_00400198;
 extern void func_weapon_004000E0(void);
@@ -12,7 +14,7 @@ void __entrypoint_func_weapon_400000(Weapon_Exports *exports) {
 
     uvUpdateFileAllocPtr(exports);
     exports->func_weapon_004000E0 = func_weapon_004000E0;
-    D_weapon_00400184 = _uvMemAllocAlign8(0x1B0U);
+    D_weapon_00400184 = _uvMemAllocAlign8(WEAPON_COUNT * sizeof(UnkStruct_weapon_00400184));
     D_weapon_00400198 = 0;
 
     for (i = 0; i < 12; i++) {
